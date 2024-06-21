6fc7fc76f4496bb425f726da77566e0212fcab1b
uyori <ryo.ando1014@gmail.com>
2024-06-21 22:16:59 +0900

    fix

--- app/frontend/main.go
+++ app/frontend/main.go
@@ -19,7 +19,7 @@ type Response struct {
 
 func main() {
 	http.HandleFunc("/frontend", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Hello!\n")
+		fmt.Fprintf(w, "Cloud build v1!\n")
 	})
 
 	http.HandleFunc("/random", func(w http.ResponseWriter, r *http.Request) {
